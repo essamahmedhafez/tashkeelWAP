8c7467f4daca2b92f3581f738b9a037a4e30ada0
Essam Hafez <Essam@Essams-MacBook-Pro.local>
2014-04-17 19:16:53 +0200

    dynamic photo adding

--- tashkeelWAP/app/controllers/Application.java
+++ tashkeelWAP/app/controllers/Application.java
@@ -12,21 +12,18 @@ import play.api.*;
 public class Application extends Controller {
 
 	
-	static User h = new User("a@a.com","a","a");
+	static User h = new User("b@a.com","a","a");
   static Words w = new Words("مدرسه","images/ImageSample.png");
+
   public static Result index() {
       //return redirect(routes.Application.login());
       //h.save();
       //w.save();
-       return ok(
-            login.render(Form.form(Login.class))
-        );
+       return ok(login.render(Form.form(Login.class)));
 	}
 
   public static Result login() {
-        return ok(
-            login.render(Form.form(Login.class))
-        );
+        return ok(login.render(Form.form(Login.class)));
     }
 
 
@@ -37,24 +34,36 @@ public static Result authenticate() {
     } else {
         session().clear();
         session("email", requestData.get("email"));
+
+
         String email = requestData.get("email");
+        
         String password = requestData.get("password");
         User temp = User.find.byId(email);
         Words word = Words.find.byId(1);
         Integer score = temp.score;
-        String link = word.imageLink;
-        return ok(user.render(email,score,link));
+        Integer wordID = word.id;
+        return ok(user.render(email,score,wordID));
     }
 }
 
-    public static Result user(String email, Integer score, String image) {//int id
+    public static Result user(String email, Integer score, Integer wordID) {//int id
     	//User user = User.findById(id);
-
-    	return ok(user.render(email,score,image));
+    	return ok(user.render(email,score,wordID));
   	}
   	
-  	public static void firstHelp() {
-        
+
+    public static Result firstHelp() {
+      return ok();
+    }
+
+
+  	public static Result sendThirdHelp() {
+      //request().email();
+      DynamicForm requestData = Form.form().bindFromRequest();
+      //Form<Task> taskForm = form(Task.class).bindFromRequest();
+      
+      return ok(login.render(Form.form(Login.class)));
     }
 
 public static class Login {
@@ -62,12 +71,12 @@ public static class Login {
     public String email;
     public String password;
 
-public String validate() {
-    if (User.authenticate(email, password) == null) {
-      return "Invalid user or password";
-    }
-    return null;
-}
+  public String validate() {
+      if (User.authenticate(email, password) == null) {
+        return "Invalid user or password";
+      }
+      return null;
+  }
 }
 
 }
